bfc2b5c1a3436095789eebfc7c03777d9f13945f
Ryan Chan <ryanchan1622@yahoo.com>
2020-10-25 14:55:48 -0700

    target aquired

--- bot.js
+++ bot.js
@@ -20,13 +20,13 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 3).toLowerCase() == 'im ') {    
+    if (userID === process.env.ANDYS_ID && message.substring(0, 3).toLowerCase() == 'im ') {    
         bot.sendMessage({
                     to: channelID,
                     message: 'Hi ' + message.slice(3) + ' I\'m dad'
                 });
      }
-     if (message.substring(0, 4).toLowerCase() == 'i\'m ') {    
+     if (userID === process.env.ANDYS_ID && message.substring(0, 4).toLowerCase() == 'i\'m ') {    
         bot.sendMessage({
                     to: channelID,
                     message: 'Hi ' + message.slice(4) + ' I\'m dad'
